b6add8fb0faf4fd86a0ec0fad23ac06f0e6b3f58
PaulNafureanu <paul.nafureanu@gmail.com>
2025-10-10 20:58:39 +0300

    Add basic board route handlers

--- server/src/routes/boards.ts
+++ server/src/routes/boards.ts
@@ -1,12 +1,72 @@
 import { Router } from "express";
 import { prisma } from "../db/prisma";
+import { IdParam } from "../validators/common";
+import { CreateBody, UpdateBody } from "../validators/boards";
 
 export const boards = Router();
 
-boards.get("/:id", async (req, res, next) => {});
+const PublicBoard = {
+  id: true,
+  roomId: true,
+  createdAt: true,
+  updatedAt: true,
+  states: true,
+};
 
-boards.post("/", async (req, res, next) => {});
+boards.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = IdParam.parse(req.params);
+    const board = await prisma.board.findUniqueOrThrow({
+      where: { id },
+      select: PublicBoard,
+    });
 
-boards.patch("/:id", async (req, res, next) => {});
+    res.status(200).json(board);
+  } catch (err) {
+    next(err);
+  }
+});
 
-boards.delete("/:id", async (req, res, next) => {});
+boards.post("/", async (req, res, next) => {
+  try {
+    const { roomId } = CreateBody.parse(req.body);
+    const board = await prisma.board.create({
+      data: { roomId },
+      select: PublicBoard,
+    });
+    res.status(201).location(`/boards/${board.id}`);
+
+    //TODO: make it also active for the room
+    //TODO: make a copy op from one room to another
+  } catch (err) {
+    next(err);
+  }
+});
+
+boards.patch("/:id", async (req, res, next) => {
+  try {
+    const { id } = IdParam.parse(req.params);
+    const { roomId } = UpdateBody.parse(req.body);
+    // This acts as a move board from one room to another
+    const board = await prisma.board.update({
+      where: { id },
+      data: { roomId },
+      select: PublicBoard,
+    });
+    res.status(200).json(board);
+
+    //TODO: if this board was the active board in a room, make a board the active board in that room
+  } catch (err) {
+    next(err);
+  }
+});
+
+boards.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = IdParam.parse(req.params);
+    await prisma.board.delete({ where: { id } });
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+});
--- server/src/validators/boards.ts
+++ server/src/validators/boards.ts
@@ -1,17 +1,7 @@
 import * as z from "zod";
-import { Id, refineFn, refineMsg } from "./common";
+import { Id } from "./common";
 
 const RoomId = Id;
-const Version = Id;
-const Payload = z.json();
 
 export const CreateBody = z.object({ roomId: RoomId }).strict();
-
-export const UpdateBody = z
-  .object({
-    roomId: RoomId.optional(),
-    version: Version.optional(),
-    payload: Payload.optional(),
-  })
-  .strict()
-  .refine(refineFn, refineMsg);
+export const UpdateBody = CreateBody;
